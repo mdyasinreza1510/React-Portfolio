03fe0816e6178fbc3ac9d3f788c00c1814a5735d
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-03 01:47:38 +0530

    added navbar

--- newportfolio/src/App.jsx
+++ newportfolio/src/App.jsx
@@ -1,8 +1,20 @@
 import Navbar from './Navbar.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx';
+import Skills from './Skills.jsx';
+import Projects from './Projects.jsx';
+import Contactme from './Contactme.jsx';
 function App(){
     return(
+        <>
         <Navbar/>
-
+        <Home/>
+        <About/>
+        <Skills/>
+        <Projects/>
+        <Contactme/>
+        </>
+     
 
 
     );
--- newportfolio/src/Navbar.jsx
+++ newportfolio/src/Navbar.jsx
@@ -1,8 +1,19 @@
 function Navbar(){
     return(
-        <h1>
-            hello
-        </h1>
+        <>
+            <nav class="navbar">
+        <div class="heading"><h3>MD YASIN REZA</h3></div>
+        <div class="link_container">
+            <a href="#home">HOME</a>
+            <a id="aboutlink" href="#about">ABOUT</a>
+            <a href="#skills">SKILLS</a>
+            <a href="#myprojects">PROJECTS</a>
+            <a href="#contactme">CONTACT ME</a>
+
+        </div>
+    </nav>
+    
+    </>
     );
 }
 export default Navbar
