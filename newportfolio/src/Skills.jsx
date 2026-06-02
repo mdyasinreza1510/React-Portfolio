99244531ae6f6582b9c2192a6eeafa1b197b7dd1
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-03 01:49:42 +0530

    added  skills page

---
+++ newportfolio/src/Skills.jsx
@@ -0,0 +1,42 @@
+function Skills(){
+   
+    return(
+    
+<>
+        <section id="skills">
+ <h2> MY SKILLS</h2>
+ <div className="main_container">
+    <div className="skill_container_out">
+        <div className="heading"><h3>LANGUAGES</h3></div>
+        
+
+<div className="skill_box">     <div className="skill">C</div>
+                            <div className="skill">C++</div>
+                            <div className="skill">HTML</div>
+                           
+                            <div className="skill">CSS</div>
+                            <div className="skill">PYTHON</div>
+                            <div className="skill">JavaScript</div>
+</div>  
+    </div>
+    <div className="skill_container_out">
+         <div className="heading"><h3>FRAMEWORKS</h3></div>
+
+    <div className="skill_box">
+
+        <div className="skill">TAILWIND</div>
+        <div className="skill">EXPRESS.JS</div>
+        <div className="skill">VUE.JS</div>
+        <div className="skill">REACT.JS</div>
+        <div className="skill">ANGULAR</div>
+        <div className="skill">DJANGO</div>
+    </div>
+
+    </div>
+ </div>
+  </section>
+        </>
+        
+    );
+} 
+export default Skills
