e182ec77eac0351ebe905a9273f19e2111228149
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-03 01:53:56 +0530

    added the contact me page

---
+++ newportfolio/src/Contactme.jsx
@@ -0,0 +1,25 @@
+function Contactme(){
+    return(
+        <>
+        <section id="contactme">
+    <h2>CONTACT ME</h2>
+    <div className="main-cont">
+       
+        <div className="second-div">
+             <h3>Get In Touch</h3>
+             <div className="input-item"><input className="name-pass" type="text"placeholder="Name" /></div>
+              <div className="input-item"><input className="name-pass" type="text"placeholder="Mail"/></div>
+               <div className="input-item"><input className="name-pass" type="text"placeholder="Your Messege"/></div>
+               <button className="sub-btn">Submit</button>
+
+
+        </div>
+       
+    </div> 
+     
+  </section>
+        
+        </>
+    );
+} 
+export default Contactme
