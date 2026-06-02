4ec502418c08338cc619aaa96682fff28a146b73
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-03 01:48:37 +0530

    added homepage

---
+++ newportfolio/src/Home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <>
+          <section id="home">
+    <h1>Hello,I am Yasin</h1>
+    <p>Web developer | Programmer | learner</p>
+    </section>
+        </>
+    );
+} 
+export default Home
