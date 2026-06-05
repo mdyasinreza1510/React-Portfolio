33d6e258a31b6fe58527dcf76b29f83138422683
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-05 22:50:40 +0530

    no

--- newportfolio/src/Home.jsx
+++ newportfolio/src/Home.jsx
@@ -1,7 +1,12 @@
+import LightRays from './Components/LightRays.jsx';
+
 function Home(){
     return(
         <>
           <section id="home">
+            
+
+
     <h1>Hello,I am Yasin</h1>
     <p>Web developer | Programmer | learner</p>
     </section>
