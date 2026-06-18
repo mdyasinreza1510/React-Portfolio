3f9d9a519b514d8a15bd498fb98eb889ca234f0a
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-18 23:51:31 +0530

    now this looks cool

--- newportfolio/src/App.jsx
+++ newportfolio/src/App.jsx
@@ -3,7 +3,7 @@ import About from './About.jsx';
 import Skill from './Skills.jsx';
 import Projects from './Projects.jsx';
 import Contactme from './Contactme.jsx';
-import heroimg from './assets/hrimage.png';
+import heroimg from './assets/herobg.png';
 import SideRays from './Components/SideRays.jsx';
 import ShinyText from './Components/ShinyText.jsx';
 import TextType from './Components/TextType.jsx';
@@ -35,37 +35,23 @@ function App() {
 
 
         <div className="hero-content">
-          <div className="mytext">
-
-            <div className="myname"><ShinyText text="MD YASIN REZA"
-              className='ogname' color='#93B1A6' speed={2} /></div>
-
-            <div className="dev-title"><ShinyText text="Web Developer |Programmer |Learner"
-              className='devtext' color='#93B1A6' speed={2.2} /></div>
-
-            <div className="bio">
+          <span id='t1' className='s1'>/// HELLO WELCOME TO MY PORTFOLIO</span>
+          <span id='t2' className='s2'>I BUILD</span>
+          <span id='t3' className='s2'>DIGITAL</span>
+          <span id='t4' className='s2'>EXPERIENCES</span>
+          <span id='t5' className='s3'>HELLO FELLAS I AM YASIN YOUR FRIEND Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vel dolore nihil dicta</span>
 
 
-              <TextType
-                text={["Hi, I’m Md Yasin Reza, a Computer Science Engineering student at Asansol Engineering College I’m passionate about building clean, user-friendly web experiences and continuously learning new technologies in the field of web development. Beyond coding, I’m a fitness enthusiast who enjoys pushing limits both in the gym and on the field. I actively follow and participate in sports and esports, finding excitement in both physical and digital arenas. These hobbies keep me disciplined, competitive, and motivated....Whether it's developing a responsive website or strategizing in an esports match, I love challenges that help me. Let’s connect and build something awesome!"]}
-                typingSpeed={20}
-                pauseDuration={1500}
-                showCursor
-                cursorCharacter="_"
-               
-                deletingSpeed={0}
-                variableSpeedEnabled={false}
-                variableSpeedMin={60}
-                variableSpeedMax={120}
-                cursorBlinkDuration={0.5}
-                loop={false}
-              />
-            </div>
-
+          <div className="herobutton">
+            <button className='hbtn'>My Work</button>
+            <button className='hbtn'>Download CV</button>
           </div>
-          <div className="mainimg">
-            <img src={heroimg} alt="pfp" />
-          </div>
+
+
+        </div>
+
+        <div className="minihero">
+
         </div>
 
       </div>
