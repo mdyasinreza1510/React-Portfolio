9ce3e7e79d62d71699d33f2a7c034ccf40bde3ea
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-22 23:23:36 +0530

    f

--- newportfolio/src/Home.jsx
+++ newportfolio/src/Home.jsx
@@ -9,55 +9,55 @@ import { User03Icon } from '@hugeicons/core-free-icons';
 
 function Home() {
   return (
-  
-      <section id='home'>
-        
-        <div className="hero-content">
-          <span id='t1' className='s1'>/// HELLO, WELCOME TO MY PORTFOLIO</span>
-          <span id='t2' className='s2'>MD</span>
-          <span id='t3' className='s2'>YASIN</span>
-          <span id='t4' className='s2'>REZA</span>
-          <span id='t5' className='s3'>HELLO FELLAS I AM YASIN YOUR FRIEND Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vel dolore nihil dicta</span>
+
+    <section id='home'>
+
+      <div className="hero-content">
+        <span id='t1' className='s1'>/// HELLO, WELCOME TO MY PORTFOLIO</span>
+        <span id='t2' className='s2'>MD</span>
+        <span id='t3' className='s2'>YASIN</span>
+        <span id='t4' className='s2'>REZA</span>
+        <span id='t5' className='s3'>HELLO FELLAS I AM YASIN YOUR FRIEND Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vel dolore nihil dicta</span>
 
 
-          <div className="herobutton">
-            <button className='hbtn'>My Work <HugeiconsIcon className='iconss' icon={WorkAlertIcon} color='#C7E113' size={20} strokeWidth={2.5} /></button>
-            <button className='hbtn'>Download CV <HugeiconsIcon icon={ArrowDown03Icon} className='iconss' size={20} color='#C7E113' strokeWidth={2.5} /></button>
-          </div>
-
-
-        </div>
-
-        <div className="minihero">
-          <div className="miniwrapper">
-            <HugeiconsIcon icon={VisualStudioCodeIcon} color='#C7E113' size={40} />
-            <span><h3>2+</h3>YEARS EXP</span></div>
-          <div className="miniwrapper">
-            <HugeiconsIcon
-              icon={BoxIcon}
-              size={40}
-              color="#C7E113"
-              strokeWidth={1.5}
-            />
-            <span><h3>10+</h3>PROJECTS</span>
-          </div>
-          <div className="miniwrapper">
-            <HugeiconsIcon
-              icon={User03Icon}
-              size={37}
-              color="#C7E113"
-              strokeWidth={1.5}
-            />
-            <span><h3>5+</h3>HAPPY CLIENTS</span>
-          </div>
-
-
+        <div className="herobutton">
+          <button className='hbtn'>My Work <HugeiconsIcon className='iconss' icon={WorkAlertIcon} color='#C7E113' size={20} strokeWidth={2.5} /></button>
+          <button className='hbtn'>Download CV <HugeiconsIcon icon={ArrowDown03Icon} className='iconss' size={20} color='#C7E113' strokeWidth={2.5} /></button>
         </div>
 
 
-      </section>
+      </div>
 
-    
+      <div className="minihero">
+        <div className="miniwrapper">
+          <HugeiconsIcon icon={VisualStudioCodeIcon} color='#C7E113' size={40} />
+          <span><h3>2+</h3>YEARS EXP</span></div>
+        <div className="miniwrapper">
+          <HugeiconsIcon
+            icon={BoxIcon}
+            size={40}
+            color="#C7E113"
+            strokeWidth={1.5}
+          />
+          <span><h3>10+</h3>PROJECTS</span>
+        </div>
+        <div className="miniwrapper">
+          <HugeiconsIcon
+            icon={User03Icon}
+            size={37}
+            color="#C7E113"
+            strokeWidth={1.5}
+          />
+          <span><h3>5+</h3>HAPPY CLIENTS</span>
+        </div>
+
+
+      </div>
+
+
+    </section>
+
+
 
   );
 }
--- newportfolio/src/Skills.jsx
+++ newportfolio/src/Skills.jsx
@@ -1,6 +1,7 @@
 import { section } from "motion/react-client";
 import { HugeiconsIcon } from '@hugeicons/react';
 import { ArrowDown03Icon } from '@hugeicons/core-free-icons';
+import { VisualStudioCodeIcon } from '@hugeicons/core-free-icons';
 
 function Skills() {
   return (
@@ -10,6 +11,10 @@ function Skills() {
           <span id='sk1' className='skl1'>// MY EXPERTIES</span>
           <span id='sk2' className='s2'>SKILLS</span>
           <span id='sk3' className='s3'>HELLO FELLAS I AM YASIN YOUR FRIEND Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vel dolore nihil dicta</span>
+          <button id="skbtn">DOWNLOAD CV</button>
+        <div className="indvdl">
+          <HugeiconsIcon icon={VisualStudioCodeIcon} color='#C7E113' size={40} />
+          <span><h3>2+</h3>YEARS EXP</span></div>
         
                       
         </div>
