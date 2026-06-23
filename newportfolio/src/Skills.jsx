83a85f88631b8ed7e026c0fa60204f8136d27ced
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-24 01:01:12 +0530

    almost complested skilll section

--- newportfolio/src/Skills.jsx
+++ newportfolio/src/Skills.jsx
@@ -9,19 +9,32 @@ function Skills() {
       <div className="skill-main-box">
         <div className="skill-info">
 
-        <span id="sk1">// MY EXPERTIES</span>
-        <span id="sk2"> SKILLS</span>
-        <span id="sk3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita veniam obcaecati iure distinctio voluptate veritatis esse molestias mod</span>
-        <button className="pagebutton ">DOWNLOAD CV <HugeiconsIcon icon={ArrowDown03Icon} className='iconss' size={20} color='#C7E113' strokeWidth={2.5} /></button>
+          <span id="sk1">// MY EXPERTIES</span>
+          <span id="sk2"> SKILLS</span>
+          <span id="sk3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita veniam obcaecati iure distinctio voluptate veritatis esse molestias mod</span>
+          <button className="pagebutton ">DOWNLOAD CV <HugeiconsIcon icon={ArrowDown03Icon} className='iconss' size={20} color='#C7E113' strokeWidth={2.5} /></button>
 
-        <div className="page-announce">
-          <span>icon</span><span><h3>2+</h3>YEARS EXPEREINCE</span>
+          <div className="page-announce">
+            <span className="ic"> <HugeiconsIcon icon={VisualStudioCodeIcon} color='#C7E113' size={45} /></span><span><h3>2+</h3>YEARS EXPEREINCE</span>
+          </div>
+
+
+
         </div>
-         
-          
-          
+        <div className="true-skills">
+          <div className="my-skill-text">// TECHNICAL SKILLS</div>
+          <div className="skill-out-box">
+            <div className="skill"></div>
+            <div className="skill"></div>
+            <div className="skill"></div>
+            <div className="skill"></div>
+            <div className="skill"></div>
+            <div className="skill"></div>
+            <div className="skill"></div>
+            <div className="skill"></div>
+            <div className="skill"></div>
+          </div>
         </div>
-        <div className="true-skills"></div>
       </div>
 
 
