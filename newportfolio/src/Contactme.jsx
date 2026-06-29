6ed0b4f542e39c96a5c8f0cb4f4837656eb7ee23
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-29 23:51:28 +0530

    working on contact me page

--- newportfolio/src/Contactme.jsx
+++ newportfolio/src/Contactme.jsx
@@ -1,25 +1,13 @@
-function Contactme(){
-    return(
+import './Styles/Contactme.css'
+function Contactme() {
+    return (
         <>
-        <section id="contactme">
-    <h2>CONTACT ME</h2>
-    <div className="main-cont">
-       
-        <div className="second-div">
-             <h3>Get In Touch</h3>
-             <div className="input-item"><input className="name-pass" type="text"placeholder="Name" /></div>
-              <div className="input-item"><input className="name-pass" type="text"placeholder="Mail"/></div>
-               <div className="input-item"><input className="name-pass" type="text"placeholder="Your Messege"/></div>
-               <button className="sub-btn">Submit</button>
+            <section id="contactme">
 
 
-        </div>
-       
-    </div> 
-     
-  </section>
-        
+            </section>
+
         </>
     );
-} 
+}
 export default Contactme
