88ae1564e427b117e1baecad35c84459968af313
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-30 01:43:57 +0530

    all layouts are done

--- newportfolio/src/Contactme.jsx
+++ newportfolio/src/Contactme.jsx
@@ -1,12 +1,23 @@
 import './Styles/Contactme.css'
+import ShinyText from './Components/ShinyText.jsx';
 function Contactme() {
     return (
         <>
             <section id="contactme">
                 <div className="headings">
-                    <span><h1>CONTACT ME</h1></span>
+                    <span className='head'><ShinyText className='cm1' text="CONTACT " color='#deff0a'
+                        delay={0.5}
+                         />
+                        <ShinyText className='cm1' text="ME" color='white'
+                        delay={0.5} /></span>
+
                     <span>LET'S CONNECT AND CREATE SOMETHING AMAZING TOGETHER</span>
                 </div>
+                <div className="cm-main-box">
+                    <div className="b1"></div>
+                    <div className="b2"></div>
+                </div>
+                <div className="footer"></div>
 
 
             </section>
