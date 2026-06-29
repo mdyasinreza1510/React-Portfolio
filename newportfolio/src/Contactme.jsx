7171720438be2122a6d826baed44056b82aab3fc
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-06-30 00:10:45 +0530

    mc

--- newportfolio/src/Contactme.jsx
+++ newportfolio/src/Contactme.jsx
@@ -3,6 +3,10 @@ function Contactme() {
     return (
         <>
             <section id="contactme">
+                <div className="headings">
+                    <span><h1>CONTACT ME</h1></span>
+                    <span>LET'S CONNECT AND CREATE SOMETHING AMAZING TOGETHER</span>
+                </div>
 
 
             </section>
